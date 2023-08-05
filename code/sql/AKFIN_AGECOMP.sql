219fe4d2a10f3d6abde75f7bfec40d2bb3308ba7
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-08-05 10:29:13 -0700

    first init, small example

---
+++ code/sql/AKFIN_AGECOMP.sql
@@ -0,0 +1,16 @@
+-- SQL Command to Create Materilized View GAP_PRODUCTS.AKFIN_AGECOMP 
+--
+-- Created by copying GAP_PRODUCTS.AGECOMP table to a materialized view 
+-- for AKFIN and does not have any other object dependencies. 
+-- GAP_PRODUCTS.AGECOMP is generated in the gap_products GitHub repo 
+-- (github.com/afsc-gap-products/gap_products) using the gapindex R package 
+-- (github.com/afsc-gap-products/gapindex). 
+--
+-- Contributors: Ned Laman (ned.laman@noaa.gov), 
+--               Zack Oyafuso (zack.oyafuso@noaa.gov), 
+--               Emily Markowitz (emily.markowitz@noaa.gov)
+--
+
+CREATE MATERIALIZED VIEW GAP_PRODUCTS.AKFIN AS 
+SELECT * FROM GAP_PRODUCTS.AGECOMP
+WHERE YEAR = 2019
