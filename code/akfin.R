c592813622c564ca2f0f5bee7ae6c2762a9fe5a5
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-08-07 23:35:52 -0700

    reworking of script
    Script loops through sql scripts to create the views and then attaches the metadata

--- code/akfin.R
+++ code/akfin.R
@@ -1,28 +1,173 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Project:       Create AKFIN Materialized Views Products
+## Description:   Create the Materialized Views in GAP_PRODUCTS that are 
+##                derivative of tables of tables in GAP_PRODUCTS, RACE_DATA,
+##                RACEBASE, V_CRUISES, etc., for AKFIN purposes. 
+##                
+##                Each materialized view has its own sql script in code/sql.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rm(list = ls())
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Import libraries and connect to Oracle
+##   Import metadata constants and commonly used functions
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(gapindex)
+sql_channel <- gapindex::get_connected()
+source("code/functions.R"); source("code/constants.R")
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Set up which AKFIN tables will be created based on which sql scripts are
+##   in code/sql. Hard code descritions of each table. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+akfin_views <- data.frame(
+  table_name = gsub(x = grep(x = dir(path = "code/sql/"), 
+                             pattern = ".sql", 
+                             value = TRUE),
+                    pattern = ".sql",
+                    replacement = ""),
+  desc = NA)
+
+akfin_views$desc[akfin_views$table_name == "AKFIN_AGECOMP"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.AGECOMP ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_AREA"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.AREA ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_BIOMASS"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.BIOMASS ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_CPUE"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.CPUE ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_HAUL"] <-
+  paste0("This table is created by subsetting the RACEBASE.HAUL table to ",
+         "only hauls with ABUNDANCE_HAUL = 'Y'. These are the hauls that ",
+         "are used for the standard production tables in GAP_PRODUCTS. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_LENGTH"] <-
+  paste0("This table is created by subsetting the RACEBASE.LENGTH table to ",
+         "only hauls with ABUNDANCE_HAUL = 'Y' from the five survey areas ",
+         "with survey_definition_id: 'AI' = 52, 'GOA' = 47, 'EBS' = 98, ",
+         "'BSS' = 78, 'NBS' = 143. These are the hauls that ",
+         "are used for the standard production tables in GAP_PRODUCTS. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_METADATA_COLUMN"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.METADATA_COLUMN ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_SIZECOMP"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.SIZECOMP ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_SPECIMEN"] <-
+  paste0("This table is created by subsetting the RACEBASE.SPECIMEN table to ",
+         "only hauls with ABUNDANCE_HAUL = 'Y' from the five survey areas ",
+         "with survey_definition_id: 'AI' = 52, 'GOA' = 47, 'EBS' = 98, ",
+         "'BSS' = 78, 'NBS' = 143. These are the hauls that ",
+         "are used for the standard production tables in GAP_PRODUCTS. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_STRATUM_GROUPS"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.STRATUM_GROUPS ",
+         "and does not have any other object dependencies. ")
+akfin_views$desc[akfin_views$table_name == "AKFIN_SURVEY_DESIGN"] <-
+  paste0("This table is a copy of GAP_PRODUCTS.SURVEY_DESIGN ",
+         "and does not have any other object dependencies. ")
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Assemble the basic text that states that GAP produced the tables, the
+##   repo that houses the code to maintain the tables, and the data the 
+##   table was created. This will be appended to each table description.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+metadata_table <- 
+  RODBC::sqlQuery(channel = sql_channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.METADATA_TABLE")
+
+table_metadata_text <- 
+  with(metadata_table,
+       paste(
+         "These data are produced",
+         METADATA_SENTENCE[METADATA_SENTENCE_NAME  == "survey_institution"],
+         METADATA_SENTENCE[METADATA_SENTENCE_NAME  == "legal_restrict"],
+         gsub(x = METADATA_SENTENCE[METADATA_SENTENCE_NAME  == "github"], 
+              pattern = "INSERT_REPO", 
+              replacement = link_repo),
+         gsub(x = METADATA_SENTENCE[METADATA_SENTENCE_NAME == "last_updated"], 
+              pattern = "INSERT_DATE", 
+              replacement = pretty_date),
+         collapse = " ", sep = " ")
+  )
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Loop over sql scripts, upload to Oracle, then append table and filed 
+##   metadata. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+metadata_fields <- 
+  RODBC::sqlQuery(channel = sql_channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.METADATA_COLUMN")
+
+for (isql_script in 
+     # nrow(x = akfin_views)
+     2:1)  {
+  start_time <- Sys.time()
+  temp_table_name <- paste0("GAP_PRODUCTS.",
+                            akfin_views$table_name[isql_script])
+  cat("Creating", temp_table_name, "...")
+  
+  available_views <-
+    subset(x = RODBC::sqlTables(channel = sql_channel, 
+                                schema = "GAP_PRODUCTS"),
+           subset = TABLE_TYPE == "VIEW")
+  
+  
+  if (akfin_views$table_name[isql_script] %in% available_views$TABLE_NAME)
+    RODBC::sqlQuery(channel = sql_channel, 
+                    query = paste("DROP VIEW", temp_table_name))
+  
+  RODBC::sqlQuery(channel = sql_channel,
+                  query = getSQL(filepath = paste0(
+                    "code/sql/",
+                    akfin_views$table_name[isql_script],
+                    ".sql")))
+  
+  temp_field_metadata <- 
+    subset(x = metadata_fields,
+           subset =  METADATA_COLNAME %in% 
+             RODBC::sqlColumns(channel = sql_channel,
+                               sqtable = temp_table_name)$COLUMN_NAME)
+  
+  update_metadata(schema = "GAP_PRODUCTS", 
+                  table_name = akfin_views$table_name[isql_script], 
+                  table_type = "MATERIALIZED VIEW",
+                  channel = sql_channel, 
+                  metadata_column = temp_field_metadata, 
+                  table_metadata = paste0(akfin_views$desc[isql_script], 
+                                          table_metadata_text))
+  
+  end_time <- Sys.time()
+  cat(names(print(end_time - start_time)), "\n")
+  
+}
 
 # assuming that they already exist
-
-akfin_views <- RODBC::sqlQuery(
-  channel = channel,
-  query = "SELECT 'Table' AS object_type, table_name
-FROM user_tables
-UNION ALL
-SELECT 'View', view_name
-FROM user_views;") %>% 
-  dplyr::filter(grepl(x = TABLE_NAME, pattern = "AKFIN_")) %>% 
-  dplyr::mutate(metadata_table = "")
-
-temp <- paste(
-  "These data were calculated using all standard hauls that can be applied to 
-  production data and abundance calculations. ", 
-  "These data are produced by ", metadata_sentence_survey_institution, 
-  metadata_sentence_legal_restrict, 
-  metadata_sentence_foss, 
-  metadata_sentence_github, 
-  metadata_sentence_codebook, 
-  metadata_sentence_last_updated, 
-  collapse = " ", sep = " ")
-
+# 
+# akfin_views <- RODBC::sqlQuery(
+#   channel = channel,
+#   query = "SELECT 'Table' AS object_type, table_name
+# FROM user_tables
+# UNION ALL
+# SELECT 'View', view_name
+# FROM user_views;") %>% 
+#   dplyr::filter(grepl(x = TABLE_NAME, pattern = "AKFIN_")) %>% 
+#   dplyr::mutate(metadata_table = "")
+# 
+# temp <- paste(
+#   "These data were calculated using all standard hauls that can be applied to 
+#   production data and abundance calculations. ", 
+#   "These data are produced by ", metadata_sentence_survey_institution, 
+#   metadata_sentence_legal_restrict, 
+#   metadata_sentence_foss, 
+#   metadata_sentence_github, 
+#   metadata_sentence_codebook, 
+#   metadata_sentence_last_updated, 
+#   collapse = " ", sep = " ")
+# 
 # for (ii in 1:nrow(akfin_views)) {
 #   
 #   metadata_table0 <- paste0(
@@ -44,77 +189,77 @@ temp <- paste(
 #     table_metadata = metadata_table0)
 #   
 # }
-
+# 
 ## Add Table Metdata -----------------------------------------------------------
-
-GAP_PRODUCT_txt <- "These tables are complete copies or subsets of the analagous internal production table in GAP_PRODUCTS, 
-but may be subset to relevant, standard, abundance-calculation worthy observations. "
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_AGECOMP"] <- 
-  paste0(get("NEW_AGECOMP_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_AREA"] <- 
-  paste0(get("NEW_AREA_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_BIOMASS"] <- 
-  paste0(get("NEW_BIOMASS_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_CATCH"] <- paste0(
-  "All haul-level catch (organism weight and count) data for all relevant, standard hauls. ",
-  temp)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_CPUE"] <- 
-  paste0(get("NEW_CPUE_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_CRUISES"] <- paste0(
-  "All cruise, vessel, and timing data for all relevant, standard cruises. ",
-  temp)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_HAUL"] <- paste0(
-  "All haul and environmental data for all relevant, standard hauls. ",
-  temp)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_LENGTH"] <- paste0(
-  "All species length and sex data for all relevant, standard hauls. ",
-  temp)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_METADATA_COLUMN"] <- 
-  paste0(get("NEW_METADATA_COLUMN_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_SIZECOMP"] <- 
-  paste0(get("NEW_SIZECOMP_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_SPECIMEN"] <- paste0(
-  "All haul-level specimen length, weight, and sex data for all relevant, standard hauls. ",
-  temp)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_STRATUM_GROUPS"] <- 
-  paste0(get("NEW_STRATUM_GROUPS_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_SURVEY_DESIGN"] <- 
-  paste0(get("NEW_SURVEY_DESIGN_COMMENT"), 
-         GAP_PRODUCT_txt)
-
-
-for (ii in 1:nrow(akfin_views)) {
-  metadata_table <- fix_metadata_table(
-    metadata_table0 = akfin_views$metadata_table[[i]],
-    name0 = akfin_views$TABLE_NAME[ii],
-    dir_out = dir_out)
-  
-  update_metadata(
-    schema = "GAP_PRODUCTS",
-    table_name = akfin_views$TABLE_NAME[ii],
-    channel = channel,
-    metadata_column = metadata_column,
-    table_metadata = metadata_table)
-}
+# 
+# GAP_PRODUCT_txt <- "These tables are complete copies or subsets of the analagous internal production tables in GAP_PRODUCTS, 
+# but may be subset to relevant, standard, abundance-calculation worthy observations. "
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_AGECOMP"] <- 
+#   paste0(get("NEW_AGECOMP_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_AREA"] <- 
+#   paste0(get("NEW_AREA_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_BIOMASS"] <- 
+#   paste0(get("NEW_BIOMASS_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_CATCH"] <- paste0(
+#   "All haul-level catch (organism weight and count) data for all relevant, standard hauls. ",
+#   temp)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_CPUE"] <- 
+#   paste0(get("NEW_CPUE_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_CRUISES"] <- paste0(
+#   "All cruise, vessel, and timing data for all relevant, standard cruises. ",
+#   temp)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_HAUL"] <- paste0(
+#   "All haul and environmental data for all relevant, standard hauls. ",
+#   temp)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_LENGTH"] <- paste0(
+#   "All species length and sex data for all relevant, standard hauls. ",
+#   temp)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_METADATA_COLUMN"] <- 
+#   paste0(get("NEW_METADATA_COLUMN_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_SIZECOMP"] <- 
+#   paste0(get("NEW_SIZECOMP_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_SPECIMEN"] <- paste0(
+#   "All haul-level specimen length, weight, and sex data for all relevant, standard hauls. ",
+#   temp)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_STRATUM_GROUPS"] <- 
+#   paste0(get("NEW_STRATUM_GROUPS_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# akfin_views$metadata_table[akfin_views$TABLE_NAME == "AKFIN_SURVEY_DESIGN"] <- 
+#   paste0(get("NEW_SURVEY_DESIGN_COMMENT"), 
+#          GAP_PRODUCT_txt)
+# 
+# 
+# for (ii in 1:nrow(akfin_views)) {
+#   metadata_table <- fix_metadata_table(
+#     metadata_table0 = akfin_views$metadata_table[[i]],
+#     name0 = akfin_views$TABLE_NAME[ii],
+#     dir_out = dir_out)
+#   
+#   update_metadata(
+#     schema = "GAP_PRODUCTS",
+#     table_name = akfin_views$TABLE_NAME[ii],
+#     channel = channel,
+#     metadata_column = metadata_column,
+#     table_metadata = metadata_table)
+# }
 
 
--- code/metadata.R
+++ code/metadata.R
@@ -1,80 +1,164 @@
-#' -----------------------------------------------------------------------------
-#' title: Create public data 
-#' author: EH Markowitz
-#' start date: 2022-01-01
-#' Notes: 
-#' -----------------------------------------------------------------------------
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Project:       Update Metadata Tables in Oracle
+## Description:   Pull the most recent version of the future oracle spreadsheet
+##                google sheet, save locally, then upload to GAP_PRODUCTS. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rm(list = ls())
 
-# dir_out <- paste0(here::here("metadata", Sys.Date()), "/")
-# dir.create(dir_out)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Import Libraries
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(googledrive)
+library(gapindex)
+library(readxl)
+library(RODBC)
+library(janitor)
+library(dplyr)
 
-# Load metadata ----------------------------------------------------------------
+sql_channel <- gapindex::get_connected()
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Download the most recent version of the future_oracle.xlsx 
+##   google sheet and save locally in the temp folder. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if (!dir.exists("temp")) dir.create(path = "temp/")
 
 # https://docs.google.com/spreadsheets/d/1wgAJPPWif1CC01iT2S6ZtoYlhOM0RSGFXS9LUggdLLA/edit?pli=1#gid=65110769
 googledrive::drive_download(
   file = googledrive::as_id("1wgAJPPWif1CC01iT2S6ZtoYlhOM0RSGFXS9LUggdLLA"), 
-  path = here::here("data", "future_oracle.xlsx"), 
+  path = "temp/future_oracle.xlsx", 
   overwrite = TRUE)
 
-# Clean metadata ----------------------------------------------------------------
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Constants
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+link_foss <- "https://www.fisheries.noaa.gov/foss"  
+link_repo <- "https://github.com/afsc-gap-products/gap_products" 
+link_repo_web <- "https://afsc-gap-products.github.io/gap_products/"
+link_code_books <- "https://www.fisheries.noaa.gov/resource/document/groundfish-survey-species-code-manual-and-data-codes-manual"
+pretty_date <- format(Sys.Date(), "%B %d, %Y")
 
-## Table -----------------------------------------------------------------------
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Clean up metadata_table: the table that houses the shared sentence 
+##   fragments that will describe each table. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+metadata_table <- readxl::read_xlsx(path = "temp/future_oracle.xlsx", 
+                                    sheet = "METADATA_TABLE") 
+metadata_table$metadata_sentence <- gsub(x = metadata_table$metadata_sentence,
+                                         pattern = "INSERT_CODE_BOOK", 
+                                         replacement = link_code_books)
 
-NEW_METADATA_TABLE <- metadata_table <- readxl::read_xlsx(
-  path = here::here("data", "future_oracle.xlsx"), 
-  sheet = "METADATA_TABLE") %>%
-  janitor::clean_names() %>% 
-  dplyr::select(-dplyr::starts_with("x"), -dplyr::starts_with("na")) %>% 
-  dplyr::filter(!is.na(metadata_sentence_name)) %>% 
-  dplyr::mutate(metadata_sentence = gsub(replacement = link_code_books, pattern = "INSERT_CODE_BOOK", x = metadata_sentence))
+shared_metadata_comment <-
+  with(metadata_table, 
+       paste(
+         metadata_sentence[metadata_sentence_name == "survey_institution"], 
+         gsub(pattern = "INSERT_REPO", replacement = link_repo, 
+              x = metadata_sentence[metadata_sentence_name == "github"]), 
+         gsub(pattern = "INSERT_DATE", replacement = pretty_date, 
+              x = metadata_sentence[metadata_sentence_name == "last_updated"]), 
+         metadata_sentence[metadata_sentence_name == "legal_restrict_none"], 
+         metadata_sentence[metadata_sentence_name == "codebook"])
+  )
 
-temp <- paste0(
-  "These tables are created ", 
-  metadata_table$metadata_sentence[metadata_table$metadata_sentence_name == "survey_institution"], " ", 
-  gsub(pattern = "INSERT_REPO", replacement = link_repo, 
-       x = metadata_table$metadata_sentence[metadata_table$metadata_sentence_name == "github"]), " ", 
-  gsub(pattern = "INSERT_DATE", replacement = pretty_date, 
-       x = metadata_table$metadata_sentence[metadata_table$metadata_sentence_name == "last_updated"]), " ", 
-  metadata_table$metadata_sentence[metadata_table$metadata_sentence_name == "legal_restrict_none"], " ", 
-  metadata_table$metadata_sentence[metadata_table$metadata_sentence_name == "codebook"], " ")
+metadata_table_comment <- paste(
+  "These columns provide the table metadata for all of the tables and ",
+  "views in GAP_PRODUCTS. These tables are created", shared_metadata_comment
+)
 
-NEW_METADATA_TABLE_COMMENT <- paste0("These column provide the column metadata for all GAP oracle tables. ", temp)
-
-## Column ----------------------------------------------------------------------
-
-NEW_METADATA_COLUMN <- readxl::read_xlsx(
-  path = here::here("data", "future_oracle.xlsx"), 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Clean up metadata_column: df that houses metadata column info
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+metadata_column <- readxl::read_xlsx( ## import spreadsheet
+  path = "temp/future_oracle.xlsx", 
   sheet = "METADATA_COLUMN", 
   skip = 1) %>%
-  janitor::clean_names() %>% 
-  dplyr::select(dplyr::starts_with("metadata_")) %>% 
-  dplyr::filter(!is.na(metadata_colname)) %>% 
+  janitor::clean_names() %>% ## clean field names
+  ## select fields that start with "metadata_"
+  dplyr::select(dplyr::starts_with("metadata_")) %>%
+  ## filter out true NAs, "", "NA", and nulls in field "metadata_colname"
+  dplyr::filter(!is.na(metadata_colname)) %>%  
   dplyr::filter(!is.null(metadata_colname)) %>% 
   dplyr::filter(!(metadata_colname %in% c("", "NA"))) %>% 
   dplyr::mutate(
-    metadata_colname_desc = gsub(pattern = "INSERT_CODE_BOOK", replacement = link_code_books, x = metadata_colname_desc), 
-    metadata_colname_desc = gsub(pattern = "  ", replacement = " ", x = metadata_colname_desc, fixed = TRUE), 
-    metadata_colname_desc = gsub(pattern = "..", replacement = ".", x = metadata_colname_desc, fixed = TRUE))
+    ## input the links to the codebook
+    metadata_colname_desc = gsub(pattern = "INSERT_CODE_BOOK", 
+                                 replacement = link_code_books, 
+                                 x = metadata_colname_desc), 
+    ## remove extra spaces?
+    metadata_colname_desc = gsub(pattern = "  ", 
+                                 replacement = " ", 
+                                 x = metadata_colname_desc, 
+                                 fixed = TRUE), 
+    ## remove extra periods?
+    metadata_colname_desc = gsub(pattern = "..", 
+                                 replacement = ".", 
+                                 x = metadata_colname_desc, 
+                                 fixed = TRUE))
 
-NEW_METADATA_COLUMN_COMMENT <- paste0("These tables provide the column metadata for all GAP oracle tables. ", temp)
+metadata_column_comment <- paste0(
+  "These tables provide the column metadata for all of the tables and ",
+  "views in GAP_PRODUCTS. These tables are created ",
+  shared_metadata_comment)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  Upload the two tables to Oracle
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+for (isql_table in c("metadata_table", 
+                     "metadata_column")) { ## loop over tables -- start
+  
+  ## Temporary dataframe that houses comment on each field The 
+  ## field names in get(x = isql_table) should match those in 
+  ## `metadata_column$metadata_colname`. If they don't, modify the future_oracle
+  ## spreadsheet and reimport. 
+  temp_metadata_column <- 
+    subset(x = metadata_column, 
+           subset = metadata_colname %in% 
+             toupper(names(x = get(x = isql_table))))
+  
+  ## In gapindex::upload_oracle, the `table_metadata` argument requires 
+  ## specific field names
+  names(x = temp_metadata_column) <- gsub(x = names(x = temp_metadata_column),
+                                          pattern = "metadata_",
+                                          replacement = "")
+  
+  ## Temporary comment on the table itself
+  temp_metadata_comment <- 
+    get(x = paste0(isql_table, "_comment"))
+  
+  ## Upload table: gapindex function will drop the table if it already exists,
+  ## saves the table, then adds the comment on the table and each column.
+  gapindex::upload_oracle(x = get(x = isql_table), 
+                          table_name = toupper(x = isql_table), 
+                          metadata_column = temp_metadata_column, 
+                          table_metadata = temp_metadata_comment, 
+                          channel = sql_channel, 
+                          schema = "GAP_PRODUCTS", 
+                          share_with_all_users = TRUE)
+  
+} ## loop over tables -- end
+
 
 # Make metadata sentences for this repo ----------------------------------------
 
-for (i in 1:nrow(NEW_METADATA_TABLE)){
-  assign(x = paste0("metadata_sentence_", NEW_METADATA_TABLE$metadata_sentence_name[i]), 
-         value = NEW_METADATA_TABLE$metadata_sentence[i])
-}
+# for (i in 1:nrow(NEW_METADATA_TABLE)){
+#   assign(x = paste0("metadata_sentence_", 
+#                     metadata_table$metadata_sentence_name[i]), 
+#          value = metadata_table$metadata_sentence[i])
+# }
+# 
+# metadata_sentence_github <- gsub(
+#   x = metadata_sentence_github, 
+#   pattern = "INSERT_REPO", 
+#   replacement = link_repo)
+# 
+# metadata_sentence_last_updated <- gsub(
+#   x = metadata_sentence_last_updated, 
+#   pattern = "INSERT_DATE", 
+#   replacement = pretty_date)
+# 
+# 
+# metadata_column <- NEW_METADATA_COLUMN
+# names(metadata_column) <- gsub(pattern = "metadata_", 
+#                                replacement = "", 
+#                                x = names(metadata_column))
 
-metadata_sentence_github <- gsub(
-  x = metadata_sentence_github, 
-  pattern = "INSERT_REPO", 
-  replacement = link_repo)
-
-metadata_sentence_last_updated <- gsub(
-  x = metadata_sentence_last_updated, 
-  pattern = "INSERT_DATE", 
-  replacement = pretty_date)
-
-
-metadata_column <- NEW_METADATA_COLUMN
-names(metadata_column) <- gsub(pattern = "metadata_", replacement = "", x = names(metadata_column))
