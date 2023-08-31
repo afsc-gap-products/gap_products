b3b63daaf12d3c37541ffa814411423e3894a15a
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2023-08-31 14:40:10 -0700

    initial stab

--- code/foss.R
+++ code/foss.R
@@ -1,16 +1,81 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Project:       Create FOSS Materialized Views Products
+## Description:   Create the Materialized Views in GAP_PRODUCTS that are 
+##                derivative of tables of tables in GAP_PRODUCTS, RACE_DATA,
+##                RACEBASE, V_CRUISES, etc., for FOSS purposes. 
+##                
+##                Each materialized view has its own sql script in code/sql.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rm(list = ls())
 
-metadata_table <- paste(
-  "These datasets, FOSS_CATCH and FOSS_HAUL, 
-when full joined by the HAULJOIN variable, 
-includes zero-filled (presence and absence) observations and
-catch-per-unit-effort (CPUE) estimates for all identified species at for index stations ", 
-  metadata_sentence_survey_institution, 
-  metadata_sentence_legal_restrict_none, 
-  metadata_sentence_foss, 
-  metadata_sentence_github, 
-  metadata_sentence_codebook, 
-  metadata_sentence_last_updated, 
-  collapse = " ", sep = " ")
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Set up which FOSS tables will be created based on which sql scripts are
+##   in code/sql. Hard code descritions of each table. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(gapindex)
+sql_channel <- gapindex::get_connected()
+source("code/constants.R")
+source("code/functions.R")
+
+table_metadata_info <- 
+  RODBC::sqlQuery(channel = sql_channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.METADATA_TABLE")
+table_metadata_bits <- table_metadata_info$METADATA_SENTENCE
+names(x = table_metadata_bits) <- table_metadata_info$METADATA_SENTENCE_NAME
+
+legal_disclaimer <- paste(table_metadata_bits["survey_institution"],
+                          table_metadata_bits["legal_restrict"],
+                          gsub(x = table_metadata_bits["github"],
+                               pattern = "INSERT_REPO",
+                               replacement = link_repo),
+                          table_metadata_bits["codebook"],
+                          gsub(x = table_metadata_bits["last_updated"],
+                               pattern = "INSERT_DATE",
+                               replacement = pretty_date))
+metadata_fields <- 
+  RODBC::sqlQuery(channel = sql_channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.METADATA_COLUMN")
+
+metadata_table <- 
+  paste("These datasets, FOSS_CATCH and FOSS_HAUL, when full joined by",
+        "the HAULJOIN variable, includes zero-filled (presence and absence)",
+        "observations and catch-per-unit-effort (CPUE) estimates for all",
+        "identified species at for index stations. These tables were created", 
+        legal_disclaimer)
+
+for (isql_script in c("FOSS_HAUL", "FOSS_CATCH")) {
+  
+  temp_table_name <- paste0("GAP_PRODUCTS.", isql_script)
+  cat("Creating", temp_table_name, "...\n")
+  
+  available_views <-
+    subset(x = RODBC::sqlTables(channel = sql_channel, 
+                                schema = "GAP_PRODUCTS"),
+           subset = TABLE_TYPE == "VIEW")
+  
+  
+  if (isql_script %in% available_views$TABLE_NAME)
+    RODBC::sqlQuery(channel = sql_channel, 
+                    query = paste("DROP VIEW", temp_table_name))
+  
+  RODBC::sqlQuery(
+    channel = sql_channel,
+    query = getSQL(filepath = paste0("code/sql_foss/", isql_script, ".sql")))
+  
+  temp_field_metadata <- 
+    subset(x = metadata_fields,
+           subset =  METADATA_COLNAME %in% 
+             RODBC::sqlColumns(channel = sql_channel,
+                               sqtable = temp_table_name)$COLUMN_NAME)
+  
+  update_metadata(schema = "GAP_PRODUCTS", 
+                  table_name = isql_script, 
+                  table_type = "MATERIALIZED VIEW",
+                  channel = sql_channel, 
+                  metadata_column = temp_field_metadata, 
+                  table_metadata = metadata_table)
+  
+}
 
 ### FOSS_CATCH -----------------------------------------------------------------
 
@@ -21,10 +86,10 @@ metadata_table <- fix_metadata_table(
 
 update_metadata(
   schema = "GAP_PRODUCTS", 
-    table_name = "FOSS_CATCH", 
-    channel = channel, 
-    metadata_column = metadata_column, 
-    table_metadata = metadata_table)
+  table_name = "FOSS_CATCH", 
+  channel = channel, 
+  metadata_column = metadata_column, 
+  table_metadata = metadata_table)
 
 ### FOSS_HAUL ------------------------------------------------------------------
 
