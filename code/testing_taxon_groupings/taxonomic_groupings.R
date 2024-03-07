360061feb18e2bc891e31f1196b1747bdf60b5c9
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-03-07 04:01:10 -0900

    adjusted to RACEBASE tables

--- code/testing_taxon_groupings/taxonomic_groupings.R
+++ code/testing_taxon_groupings/taxonomic_groupings.R
@@ -10,7 +10,7 @@ rm(list = ls())
 library(googledrive)
 library(gapindex)
 
-chl <- gapindex::get_connected()
+chl <- gapindex::get_connected(check_access = F)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Download spreadsheet where the tables currently exist (in the future,
@@ -28,7 +28,7 @@ data_spreadsheet <- googledrive::drive_download(file = data_id,
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 GROUPED_TAXA <- 
   subset(x = as.data.frame(readxl::read_excel(path = "temp/data.xlsx", 
-                                              sheet = "TAXONOMIC_GROUPING")),
+                                              sheet = "TAXONOMIC_GROUPING_RB")),
          subset = stage == 1)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -42,10 +42,12 @@ for (irow in 1:nrow(x = GROUPED_TAXA)) {
   temp_grab <- 
     RODBC::sqlQuery(channel = chl,
                     query = paste0("SELECT *
-                                    FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                    WHERE SURVEY_SPECIES = 1 AND ", 
-                                    GROUPED_TAXA$LOWEST_TAXONOMIC_ID[irow],
-                                   "_TAXON = '", GROUPED_TAXA$LOWEST_TAXONOMIC_NAME[irow], "'"))
+                                    FROM RACEBASE.SPECIES_CLASSIFICATION 
+                                    WHERE ", 
+                                   GROUPED_TAXA$LOWEST_TAXONOMIC_ID[irow],
+                                   "_TAXON = '",
+                                   GROUPED_TAXA$LOWEST_TAXONOMIC_NAME[irow], 
+                                   "'"))
   
   temp_grab$GROUP <- GROUPED_TAXA$SPECIES_CODE[irow]
   grouped_taxa_df <- rbind(grouped_taxa_df, temp_grab)
@@ -54,78 +56,123 @@ for (irow in 1:nrow(x = GROUPED_TAXA)) {
 
 ## Separate sea pens from Octocorallia 
 grouped_taxa_df$GROUP[grouped_taxa_df$SUPERFAMILY_TAXON == 'Pennatuloidea'] <- 42000
-## Seprate Echiura from Polychaetes
-grouped_taxa_df$GROUP[grouped_taxa_df$SUBCLASS_TAXON == 'Echiura'] <- 94500
 ## Separate basket stars from brittle stars
-grouped_taxa_df$GROUP[grouped_taxa_df$ORDER_TAXON == "Euryalida"] <- 83020
+grouped_taxa_df$GROUP[grouped_taxa_df$SUBORDER_TAXON == "Euryalina"] <- 83020
 
-## Extract all vertebrates 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Stage Two: All fish species_codes and invertebrates where the minimum
+##   ID standard on deck is to species. First, fish.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Extract all vertebrates from RACEBASE.SPECIES_CLASSIFICATION
 fish_taxa <-
   RODBC::sqlQuery(channel = chl,
-                  query = paste0("SELECT *
-                                FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                WHERE SURVEY_SPECIES = 1 AND SUBPHYLUM_TAXON = 'Vertebrata' 
-                                   "))
+                  query = "SELECT *
+                           FROM RACEBASE.SPECIES_CLASSIFICATION 
+                           WHERE SUBPHYLUM_TAXON = 'Vertebrata'")
 fish_taxa$GROUP = fish_taxa$SPECIES_CODE
 
-# Remove Lycodes and myctophids
+# Remove Myctophids
 fish_taxa <- subset(fish_taxa,
-                    subset = GENUS_TAXON != 'Lycodes')
+                    subset = FAMILY_TAXON != "Myctophidae" | is.na(x = FAMILY_TAXON))
+
+## Separate Lycodapus eelpouts and group into Lycodapus sp. (24230), then
+## remove from fish_taxa
+lycodapus <- subset(x = fish_taxa, subset = GENUS_TAXON == "Lycodapus")
+lycodapus$GROUP <- 24230
 fish_taxa <- subset(fish_taxa,
-                    subset = FAMILY_TAXON != "Myctophidae")
+                    subset = GENUS_TAXON != "Lycodapus" | is.na(x = GENUS_TAXON))
 
-lycodes <-
-  RODBC::sqlQuery(channel = chl,
-                  query = paste0("SELECT *
-                                FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                WHERE SURVEY_SPECIES = 1 AND GENUS_TAXON = 'Lycodes'"))    
-lycodes$GROUP = 24180
-
-# myctophids <-
-#   RODBC::sqlQuery(channel = chl,
-#                   query = paste0("SELECT *
-#                                 FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-#                                 WHERE SURVEY_SPECIES = 1 AND FAMILY_TAXON = 'Myctophidae'"))    
-
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Then, invertebrates.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Extract all squids
 squids <- 
   RODBC::sqlQuery(channel = chl,
                   query = paste0("SELECT *
-                                FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                WHERE SURVEY_SPECIES = 1 AND SUPERORDER_TAXON = 'Decapodiformes'"))  
+                                  FROM RACEBASE.SPECIES_CLASSIFICATION
+                                  WHERE SUPERORDER_TAXON = 'Decapodiformes'"))  
 squids$GROUP <- squids$SPECIES_CODE
 
-## Extract hermit crabs minus the commercial ones
+## Extract hermit crabs
 hermit_crabs <-
   RODBC::sqlQuery(channel = chl, 
-                  query = paste0("SELECT *
-                                 FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                 WHERE SURVEY_SPECIES = 1 AND SUPERFAMILY_TAXON = 'Paguroidea' 
-                                 AND GENUS_TAXON != 'Paralithodes'"))  
+                  query = "SELECT *
+                           FROM RACEBASE.SPECIES_CLASSIFICATION 
+                           WHERE FAMILY_TAXON = 'Paguridae'")
 hermit_crabs$GROUP <- hermit_crabs$SPECIES_CODE
 
 ## Extract true crabs minus the commercial ones
 true_crabs <-
   RODBC::sqlQuery(channel = chl, 
-                  query = paste0("SELECT *
-                                 FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                 WHERE SURVEY_SPECIES = 1 AND Infraorder_TAXON = 'Brachyura'
-                                 AND GENUS_TAXON != 'Chionoecetes'"))  
+                  query = "SELECT *
+                           FROM RACEBASE.SPECIES_CLASSIFICATION 
+                           WHERE INFRAORDER_TAXON = 'Brachyura'
+                           AND GENUS_TAXON != 'Chionoecetes'")  
 true_crabs$GROUP <- true_crabs$SPECIES_CODE
 
+## Extract sand dollar SPECIES_CODEs
 sand_dollars <-
   RODBC::sqlQuery(channel = chl, 
-                  query = paste0("SELECT *
-                                 FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                 WHERE SURVEY_SPECIES = 1 AND order_TAXON = 'Echinolampadacea'"))
+                  query = "SELECT *
+                           FROM RACEBASE.SPECIES_CLASSIFICATION 
+                           WHERE SUPERORDER_TAXON = 'Gnathostomata'")
 sand_dollars$GROUP <- sand_dollars$SPECIES_CODE
 
+## Extract sea star SPECIES_CODEs
 sea_stars <- 
   RODBC::sqlQuery(channel = chl, 
-                  query = paste0("SELECT *
-                                 FROM GAP_PRODUCTS.TAXONOMIC_CLASSIFICATION 
-                                 WHERE SURVEY_SPECIES = 1 AND CLASS_TAXON = 'Asteroidea'"))
+                  query = "SELECT *
+                           FROM RACEBASE.SPECIES_CLASSIFICATION 
+                           WHERE CLASS_TAXON = 'Asteroidea'")
 sea_stars$GROUP <- sea_stars$SPECIES_CODE
 
-nrow(rbind(grouped_taxa_df, fish_taxa, lycodes, squids, hermit_crabs, 
-           true_crabs, sand_dollars, sea_stars))
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Which SPECIES_CODEs in RACEBASE.SPECIES_CLASSIFICATION belong to a 
+##   GROUP (i.e., either in a complex or presented individually) and which
+##   SPECIES_CODEs are left out for now. 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+all_spp_codes <- 
+  RODBC::sqlQuery(channel = chl, 
+                  query = "SELECT *
+                           FROM RACEBASE.SPECIES_CLASSIFICATION")
+
+all_taxa <- rbind(grouped_taxa_df, fish_taxa, lycodapus, squids, hermit_crabs, 
+                  true_crabs, sand_dollars, sea_stars)
+all_taxa <- merge(x = all_spp_codes,
+                  all.x = TRUE,
+                  y = all_taxa[, c("SPECIES_CODE", "GROUP")],
+                  by = "SPECIES_CODE")
+all_taxa[, names(all_taxa)[c(ncol(x = all_taxa), 1:(ncol(x = all_taxa) - 1))]]
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Upload to Oracle
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## Assemble column metadata
+metadata_column_info <-   
+  RODBC::sqlQuery(channel = chl, 
+                  query = paste0("SELECT *
+                                  FROM GAP_PRODUCTS.METADATA_COLUMN
+                                  WHERE METADATA_COLNAME IN ",
+                                 gapindex::stitch_entries(names(x = all_taxa))))
+names(x = metadata_column_info) <- 
+  gsub(x = tolower(x = names(x = metadata_column_info) ),
+       pattern = "metadata_", 
+       replacement = "")
+
+metadata_column_info <- 
+  rbind(data.frame(colname = "GROUP",
+                   colname_long = "SPECIES_CODE or COMPLEX_CODE",
+                   units = "ID key code",
+                   datatype = "NUMBER(38,0)",
+                   colname_desc = "Code that is either associated with an indivdual species code or the complex to which that taxon belongs."),
+        metadata_column_info)
+
+## Upload to Oracle
+gapindex::upload_oracle(x = all_taxa,
+                        table_name = "TAXON_GROUPS", 
+                        metadata_column = metadata_column_info, 
+                        table_metadata = "This lookup table is a copy of RACEBASE.SPECIES_CLASSIFICATION but with an added field GROUP to identify species complexes. To be used in the production of the GAP_PRODUCTS tables.", 
+                        channel = chl, 
+                        schema = "GAP_PRODUCTS", 
+                        share_with_all_users = TRUE)
