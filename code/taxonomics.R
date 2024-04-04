8d25b2d853e87ad8769e60da24d365446751d541
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-04-04 16:13:47 -0700

    started reworking

--- code/taxonomics.R
+++ code/taxonomics.R
@@ -33,49 +33,79 @@ if (file.exists("Z:/Projects/ConnectToOracle.R")) {
   source("Z:/Projects/ConnectToOracle.R")
   channel <- channel_products
 } else {
-  channel <- gapindex::get_connected()
+  channel <- gapindex::get_connected(check_access = F)
 }
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Download the most recent version of the future_oracle.xlsx 
-##   google sheet and save locally in the temp folder. 
+##   Download the most recent version of the taxonomic confidence tables
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 googledrive::drive_auth()
-2
 
 a <- googledrive::drive_ls(path = "https://drive.google.com/drive/folders/1s-BKOnfiuF3b0642C_DGhLcuRv-MOPxO")
-for (i in 1:nrow(a)) {
-googledrive::drive_download(
-  file = a$id[i], 
-  path = paste0("temp/", a$name[i]), 
-  overwrite = TRUE)
+
+for (i in 1:nrow(x = a)) {
+  googledrive::drive_download(file = a$id[i], 
+                              path = paste0("temp/", a$name[i]), 
+                              overwrite = TRUE)
 }
 
+current_year <- as.integer(format(x = Sys.Date(), format = "%Y"))
+survey_years <- 
+  RODBC::sqlQuery(channel = channel, 
+                  query = "SELECT * FROM GAP_PRODUCTS.SURVEY_DESIGN")
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Load original taxon confidence data
+##   For each region, 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 TAXONOMIC_CONFIDENCE <- data.frame()
-a <- a$name
-for (i in 1:length(a)){
-  print(a[i])
-  b <- readxl::read_xlsx(path = here::here("temp/", a[i]), 
-                         skip = 1, col_names = TRUE) %>% 
+
+regions <- a$name
+for (iregion in 1:length(x = regions)) {
+  print(regions[iregion])
+  
+  ## Import the original taxonomic confidence region for regions[iregion]
+  taxon_conf_wide <- 
+    readxl::read_xlsx(path = here::here("temp/", regions[iregion]), 
+                      skip = 1, col_names = TRUE) %>% 
     dplyr::select(where(~!all(is.na(.x)))) %>% # remove empty columns
     janitor::clean_names() %>% 
     dplyr::rename(species_code = code)
-  if (sum(names(b) %in% "quality_codes")>0) {
-    b$quality_codes<-NULL
+  
+  ## If the field 'quality_codes' is not in `taxon_conf_wide`, create the field
+  if (!("quality_codes" %in% names(x = taxon_conf_wide))) 
+    taxon_conf_wide$quality_codes<-NULL
+  
+  ## What is the most current year of data?
+  last_year <- max(as.numeric(x = gsub(x = grep(x = names(x = taxon_conf_wide), 
+                                                pattern = "x", 
+                                                value = T),
+                                       pattern = "x",
+                                       replacement = "")))
+  
+  ## Extract region name
+  region_name <- strsplit(x = gsub(x = gsub(x = regions[iregion], 
+                                            pattern = "TAXONOMIC_CONFIDENCE_", 
+                                            replacement = ""), 
+                                   pattern = ".xlsx", 
+                                   replacement = ""), 
+                          split = "_")[[1]][[3]]
+  
+  ## If the taxonomic confidence tables haven't been updated, fill in 
+  if (last_year < current_year) {
+    imputted_years <- 
+      subset(x = survey_years, 
+             subset = YEAR > last_year & 
+               SURVEY_DEFINITION_ID ==  c("AI" = 52, "GOA" = 47, "EBS" = 98, 
+                                          "BSS" = 78, "NBS" = 143)[region_name])
+    
+    if (nrow(x = imputted_years) > 0) {
+      imputted_years$YEAR
+    }
   }
   
-  cc <- strsplit(x = gsub(x = gsub(x = a[i], 
-                                   pattern = "TAXONOMIC_CONFIDENCE_", replacement = ""), 
-                          pattern = ".xlsx", 
-                          replacement = ""), 
-                 split = "_")[[1]][[3]]
   
-  b <- b %>% 
+  taxon_conf_wide <- taxon_conf_wide %>% 
     dplyr::select(-scientific_name, -common_name) %>% 
     tidyr::pivot_longer(cols = starts_with("x"), 
                         names_to = "year", 
@@ -87,7 +117,7 @@ for (i in 1:length(a)){
                               x = year), 
                   year = as.numeric(year)) %>% 
     dplyr::distinct() %>% 
-    dplyr::mutate(SRVY = cc)
+    dplyr::mutate(SRVY = region_name)
   
   TAXONOMIC_CONFIDENCE <- TAXONOMIC_CONFIDENCE %>% 
     dplyr::bind_rows(b)
@@ -113,7 +143,10 @@ TAXONOMIC_CONFIDENCE <- TAXONOMIC_CONFIDENCE %>%
                    by = "SRVY") %>% 
   dplyr::select(-SRVY)
 
-### fill in TAXONOMIC_CONFIDENCE with, if missing, the values from the year before -------
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Fill in TAXONOMIC_CONFIDENCE with, if missing, the values from the prior year
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 comb1 <- RODBC::sqlQuery(channel = channel, 
                          query = "SELECT * FROM GAP_PRODUCTS.AKFIN_CRUISE") %>% 
@@ -136,9 +169,9 @@ for (i in 1:nrow(comb)) {
   TAXONOMIC_CONFIDENCE <- dplyr::bind_rows(
     TAXONOMIC_CONFIDENCE, 
     TAXONOMIC_CONFIDENCE %>% 
-    dplyr::filter(survey_definition_id == comb$survey_definition_id[i] & 
-                    year == max(year, na.rm = TRUE)) %>% 
-    dplyr::mutate(year = as.numeric(comb$year[i])) )
+      dplyr::filter(survey_definition_id == comb$survey_definition_id[i] & 
+                      year == max(year, na.rm = TRUE)) %>% 
+      dplyr::mutate(year = as.numeric(comb$year[i])) )
 }
 
 ### Add table and column metadata -------
@@ -150,18 +183,18 @@ TAXONOMIC_CONFIDENCE_COMMENT <- paste0(
     and is intended to serve as a general guideline for data users interested in 
     assessing the relative reliability of historical species identifications 
     on these surveys. ", 
-    # "This dataset includes an identification confidence matrix 
-    # for all fishes and invertebrates identified ", 
+  # "This dataset includes an identification confidence matrix 
+  # for all fishes and invertebrates identified ", 
   # metadata_sentence_survey_institution, 
   "Quality Codes: ", 
   "1: High confidence and consistency. Taxonomy is stable and reliable at this level, and field identification characteristics are well known and reliable. ",
   "2: Moderate confidence. Taxonomy may be questionable at this level, or field identification characteristics may be variable and difficult to assess consistently. ", 
   "3: Low confidence. Taxonomy is incompletely known, or reliable field identification characteristics are unknown. ", 
   "NA: Unassessed. Taxonomy quality has not been assessed. ")#, 
-  # metadata_sentence_legal_restrict, " ",  
-  # metadata_sentence_github, " ", 
-  # metadata_sentence_codebook, " ", 
-  # metadata_sentence_last_updated)
+# metadata_sentence_legal_restrict, " ",  
+# metadata_sentence_github, " ", 
+# metadata_sentence_codebook, " ", 
+# metadata_sentence_last_updated)
 
 write.csv(x = TAXONOMIC_CONFIDENCE,
           file = paste0("temp/taxonomics_confidence.csv"),
