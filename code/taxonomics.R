6db9c19d77d28a83cadced0a77b689cf68f9f791
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-04-05 18:59:24 -0700

    See description
    - added more comments, argument names
    - using more informative variable names
    - only providing taxonomic confidence for years that a survey occurred
    - removed null survey_definition_ids
    - added uploading function

--- code/taxonomics.R
+++ code/taxonomics.R
@@ -1,8 +1,8 @@
 #' -----------------------------------------------------------------------------
-#' title: Taxonomic ID Confidence Tables
-#' author: EH Markowitz
-#' Notes: This script creates the taxonomic identification confidence tables
-#'        for each region and survey year used currently for FOSS purposes. 
+#' Title:   Creation of the GAP_PRODUCTS.TAXONOMIC_CONFIDENCE table
+#' Authors: EH Markowitz, Zack Oyafuso
+#' Notes:   This script creates the taxonomic identification confidence tables
+#'          for each region and survey year used currently for FOSS purposes. 
 #'        
 #'        Quality Codes:
 #'        1 – High confidence and consistency. Taxonomy is stable and reliable 
@@ -13,6 +13,8 @@
 #'            difficult to assess consistently.
 #'        3 – Low confidence. Taxonomy is incompletely known, or reliable field
 #'            identification characteristics are unknown.
+#'            
+#'        Source for taxonomic tables:    
 #' -----------------------------------------------------------------------------
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -20,186 +22,222 @@
 ##   Connect to Oracle (Make sure to connect to network or VPN)
 ##   Be sure to use the username and password for the GAP_PRODUCTS schema
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(dplyr)
+library(readxl)
+library(tidyr)
+library(googledrive)
+library(gapindex)
 
-PKG <- c("dplyr", "magrittr", "readxl", "tidyr", "googledrive")
-for (p in PKG) {
-  if (!require(p, character.only = TRUE)) {
-    install.packages(p)
-    require(p, character.only = TRUE)
-  }
-}
-
-if (file.exists("Z:/Projects/ConnectToOracle.R")) {
-  source("Z:/Projects/ConnectToOracle.R")
-  channel <- channel_products
-} else {
-  channel <- gapindex::get_connected(check_access = F)
-}
+channel <- gapindex::get_connected(check_access = F)
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Download the most recent version of the taxonomic confidence tables
+##   Download the most recent version of the taxonomic confidence tables for 
+##   each region from a googledrive folder. There is no NBS taxonomic 
+##   confidence table so we assume that the taxonomic confidence table of the
+##   NBS is the same as the EBS. The full link to the folder is:
+##   https://drive.google.com/drive/folders/1s-BKOnfiuF3b0642C_DGhLcuRv-MOPxO
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 googledrive::drive_auth()
 
-a <- googledrive::drive_ls(path = "https://drive.google.com/drive/folders/1s-BKOnfiuF3b0642C_DGhLcuRv-MOPxO")
+data_sources <- googledrive::drive_ls(
+  path = googledrive::as_id(x = "1s-BKOnfiuF3b0642C_DGhLcuRv-MOPxO"))
 
-for (i in 1:nrow(x = a)) {
-  googledrive::drive_download(file = a$id[i], 
-                              path = paste0("temp/", a$name[i]), 
+for (idata in 1:nrow(x = data_sources)) {
+  googledrive::drive_download(file = data_sources$id[idata], 
+                              path = paste0("temp/", data_sources$name[idata]), 
                               overwrite = TRUE)
 }
 
-current_year <- as.integer(format(x = Sys.Date(), format = "%Y"))
-survey_years <- 
-  RODBC::sqlQuery(channel = channel, 
-                  query = "SELECT * FROM GAP_PRODUCTS.SURVEY_DESIGN")
-
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   For each region, 
+##   For each region, import the taxonomic confidence spreadsheet, clean up, 
+##   and append to a data.frame called `TAXONOMIC_CONFIDENCE`
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+current_year <- as.integer(x = format(x = Sys.Date(), format = "%Y"))
+survey_years <- RODBC::sqlQuery(channel = channel,
+                                query = "SELECT * 
+                                         FROM GAP_PRODUCTS.SURVEY_DESIGN")
 
 TAXONOMIC_CONFIDENCE <- data.frame()
-
-regions <- a$name
-for (iregion in 1:length(x = regions)) {
-  print(regions[iregion])
+for (iregion in c("GOA", "AI", "EBS", "BSS")) {
   
-  ## Import the original taxonomic confidence region for regions[iregion]
-  taxon_conf_wide <- 
-    readxl::read_xlsx(path = here::here("temp/", regions[iregion]), 
-                      skip = 1, col_names = TRUE) %>% 
+  ## Import the original taxonomic confidence region for iregion
+  taxon_conf_wide <- readxl::read_excel(path = paste0("temp/Taxon_confidence_", 
+                                                      iregion, ".xlsx"), 
+                                        skip = 1, 
+                                        col_names = TRUE) %>% 
     dplyr::select(where(~!all(is.na(.x)))) %>% # remove empty columns
     janitor::clean_names() %>% 
-    dplyr::rename(species_code = code)
+    dplyr::rename(SPECIES_CODE = code)
   
-  ## If the field 'quality_codes' is not in `taxon_conf_wide`, create the field
-  if (!("quality_codes" %in% names(x = taxon_conf_wide))) 
-    taxon_conf_wide$quality_codes<-NULL
+  ## The quality_code field is inconsistently provided across tables so 
+  ## we remove it if it exists in the imported spreadsheet
+  if ("quality_codes" %in% names(x = taxon_conf_wide))
+    taxon_conf_wide <- subset(x = taxon_conf_wide, select = -quality_codes)
   
-  ## What is the most current year of data?
-  last_year <- max(as.numeric(x = gsub(x = grep(x = names(x = taxon_conf_wide), 
-                                                pattern = "x", 
-                                                value = T),
-                                       pattern = "x",
-                                       replacement = "")))
+  ## What is the most current year of taxonomic confidence data?
+  last_year <- 
+    max(as.numeric(x = substr(x = grep(x = names(x = taxon_conf_wide), 
+                                       pattern = "x", 
+                                       value = T),
+                              start = 2, stop = 5)))
+  last_year_idx <- 
+    which.max(x = as.numeric(x = substr(x = grep(x = names(x = taxon_conf_wide), 
+                                                 pattern = "x", 
+                                                 value = T),
+                                        start = 2, stop = 5)))
   
-  ## Extract region name
-  region_name <- strsplit(x = gsub(x = gsub(x = regions[iregion], 
-                                            pattern = "TAXONOMIC_CONFIDENCE_", 
-                                            replacement = ""), 
-                                   pattern = ".xlsx", 
-                                   replacement = ""), 
-                          split = "_")[[1]][[3]]
-  
-  ## If the taxonomic confidence tables haven't been updated, fill in 
+  ## If the taxonomic confidence table has not been updated (i.e., last_year <
+  ## current_year), impute the taxon confidence for the survey years after the
+  ## last_year using the most recent year in the taxonomic confidence table
   if (last_year < current_year) {
     imputted_years <- 
       subset(x = survey_years, 
-             subset = YEAR > last_year & 
+             subset = YEAR > last_year & YEAR <= current_year &
                SURVEY_DEFINITION_ID ==  c("AI" = 52, "GOA" = 47, "EBS" = 98, 
-                                          "BSS" = 78, "NBS" = 143)[region_name])
+                                          "BSS" = 78, "NBS" = 143)[iregion])
     
-    if (nrow(x = imputted_years) > 0) {
-      imputted_years$YEAR
-    }
+    if (nrow(x = imputted_years) > 0) 
+      taxon_conf_wide[, paste0("x", imputted_years$YEAR, "_0")] <- 
+        taxon_conf_wide[, last_year_idx]  
   }
   
-  
-  taxon_conf_wide <- taxon_conf_wide %>% 
+  ## Lengthen the wide-version taxon_conf_wide, which creates the YEAR field
+  taxon_conf_long <- taxon_conf_wide %>% 
     dplyr::select(-scientific_name, -common_name) %>% 
     tidyr::pivot_longer(cols = starts_with("x"), 
-                        names_to = "year", 
-                        values_to = "TAXONOMIC_CONFIDENCE") %>% 
-    dplyr::mutate(year = gsub(pattern = "[a-z]", 
+                        names_to = "YEAR", 
+                        values_to = "TAXON_CONFIDENCE") %>% 
+    dplyr::mutate(YEAR = gsub(pattern = "[a-z]", 
                               replacement = "", 
-                              x = year), 
-                  year = gsub(pattern = "_0", replacement = "", 
-                              x = year), 
-                  year = as.numeric(year)) %>% 
+                              x = YEAR), 
+                  YEAR = gsub(pattern = "_0", replacement = "", 
+                              x = YEAR), 
+                  YEAR = as.numeric(YEAR)) %>% 
     dplyr::distinct() %>% 
-    dplyr::mutate(SRVY = region_name)
+    dplyr::mutate(SRVY = iregion)
   
-  TAXONOMIC_CONFIDENCE <- TAXONOMIC_CONFIDENCE %>% 
-    dplyr::bind_rows(b)
+  ## Filter only years where a bottom trawl survey occurred
+  taxon_conf_long <- 
+    subset(x = taxon_conf_long, 
+           subset = YEAR %in% survey_years$YEAR[
+             survey_years$SURVEY_DEFINITION_ID == c("AI" = 52, "GOA" = 47, 
+                                                    "EBS" = 98,  "BSS" = 78, 
+                                                    "NBS" = 143)[iregion]])
   
-  if (cc == "EBS") {
+  ## Append to TAXONOMIC_CONFIDENCE
+  TAXONOMIC_CONFIDENCE <- rbind(TAXONOMIC_CONFIDENCE, taxon_conf_long)
+  
+  ## Since there is no NBS confidence table, duplicate the EBS table
+  if (iregion == "EBS") 
     TAXONOMIC_CONFIDENCE <- TAXONOMIC_CONFIDENCE %>% 
-      dplyr::bind_rows(b %>% 
-                         dplyr::mutate(SRVY = "NBS"))
-  }
+    dplyr::bind_rows(subset(x = taxon_conf_long,
+                            subset = YEAR %in% survey_years$YEAR[
+                              survey_years$SURVEY_DEFINITION_ID == 143
+                            ]) %>% 
+                       dplyr::mutate(SRVY = "NBS")
+    )
 }
 
+## Add TAXON_CONFIDENCE field which is a text version of TAXON_CONFIDENCE_CODE
 TAXONOMIC_CONFIDENCE <- TAXONOMIC_CONFIDENCE %>% 
-  dplyr::mutate(TAXONOMIC_CONFIDENCE_code = TAXONOMIC_CONFIDENCE, 
-                TAXONOMIC_CONFIDENCE = dplyr::case_when(
-                  TAXONOMIC_CONFIDENCE_code == 1 ~ "High",
-                  TAXONOMIC_CONFIDENCE_code == 2 ~ "Moderate",
-                  TAXONOMIC_CONFIDENCE_code == 3 ~ "Low",
+  dplyr::mutate(TAXON_CONFIDENCE_CODE = TAXON_CONFIDENCE, 
+                TAXON_CONFIDENCE = dplyr::case_when(
+                  TAXON_CONFIDENCE_CODE == 1 ~ "High",
+                  TAXON_CONFIDENCE_CODE == 2 ~ "Moderate",
+                  TAXON_CONFIDENCE_CODE == 3 ~ "Low",
                   TRUE ~ "Unassessed")
   ) %>%
   dplyr::left_join(y = 
-                     data.frame(survey_definition_id = c(143, 98, 47, 52, 78),
+                     data.frame(SURVEY_DEFINITION_ID = c(143, 98, 47, 52, 78),
                                 SRVY = c("NBS", "EBS", "GOA", "AI", "BSS") ),
                    by = "SRVY") %>% 
   dplyr::select(-SRVY)
 
-
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Fill in TAXONOMIC_CONFIDENCE with, if missing, the values from the prior year
+##   For survey years without taxonomic confidence data, impute taxonomic 
+##   confidence values from the previous survey year for that survey
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+missing_survey_years <- dplyr::anti_join(
+  x = subset(survey_years, YEAR <= current_year),
+  y = unique(x = TAXONOMIC_CONFIDENCE[, c("SURVEY_DEFINITION_ID", "YEAR")]),
+  by = c("SURVEY_DEFINITION_ID", "YEAR"))
 
-comb1 <- RODBC::sqlQuery(channel = channel, 
-                         query = "SELECT * FROM GAP_PRODUCTS.AKFIN_CRUISE") %>% 
-  janitor::clean_names() %>% 
-  dplyr::filter(survey_definition_id %in% c(143, 98, 47, 52, 78) & 
-                  !is.na(cruisejoin) & 
-                  year >= 1982 &
-                  !is.na(year)) %>% 
-  dplyr::select(year, survey_definition_id) %>% 
-  dplyr::distinct()
-
-comb2 <- unique(TAXONOMIC_CONFIDENCE[, c("survey_definition_id", "year")])
-comb1$comb <- paste0(comb1$survey_definition_id, "_", comb1$year)
-comb2$comb <- paste0(comb2$survey_definition_id, "_", comb2$year)
-comb <- strsplit(x = setdiff(comb1$comb, comb2$comb), split = "_")
-comb <- data.frame(t(data.frame(comb)))
-names(comb) <- c("survey_definition_id", "year")
-
-for (i in 1:nrow(comb)) {
+for (iyear in 1:nrow(x = missing_survey_years)) {
+  
+  ## Query unique survey years for a given survey prior to the missing year
+  temp_survey_year <- 
+    subset(x = unique(x = TAXONOMIC_CONFIDENCE[, c("SURVEY_DEFINITION_ID", 
+                                                   "YEAR")]),
+           subset = SURVEY_DEFINITION_ID == 
+             missing_survey_years$SURVEY_DEFINITION_ID[iyear] &
+             YEAR < missing_survey_years$YEAR[iyear])
+  
+  ## The year to impute the missing year's data will be the survey year prior
+  ## to the missing year
+  imputted_year <- temp_survey_year[which.max(x = temp_survey_year$YEAR), ]
+  
+  ## Append the imputted year's taxonomic confidence data for that survey
+  ## to the TAXONOMIC_CONFIDENCE data frame.  
   TAXONOMIC_CONFIDENCE <- dplyr::bind_rows(
     TAXONOMIC_CONFIDENCE, 
     TAXONOMIC_CONFIDENCE %>% 
-      dplyr::filter(survey_definition_id == comb$survey_definition_id[i] & 
-                      year == max(year, na.rm = TRUE)) %>% 
-      dplyr::mutate(year = as.numeric(comb$year[i])) )
+      dplyr::filter(SURVEY_DEFINITION_ID == imputted_year$SURVEY_DEFINITION_ID & 
+                      YEAR == imputted_year$YEAR) %>% 
+      dplyr::mutate(YEAR = missing_survey_years$YEAR[iyear]) 
+  )
 }
 
-### Add table and column metadata -------
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##    Filter for SPECIES_CODE values present in the GROUP_CODE field in 
+##    GAP_PRODUCTS.TAXON_GROUPS   
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+spp_codes <- 
+  RODBC::sqlQuery(channel = channel, 
+                  query = "SELECT DISTINCT (GROUP_CODE) 
+                           FROM GAP_PRODUCTS.TAXON_GROUPS 
+                           ORDER BY GROUP_CODE")$GROUP_CODE
+TAXONOMIC_CONFIDENCE <- subset(x = TAXONOMIC_CONFIDENCE, 
+                               subset = SPECIES_CODE %in% spp_codes)
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   Create the table and column metadata
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 TAXONOMIC_CONFIDENCE_COMMENT <- paste0(
   "The quality and specificity of field identifications for many taxa have 
-    fluctuated over the history of the surveys due to changing priorities and resources. 
-    The matrix lists a confidence level for each taxon for each survey year 
-    and is intended to serve as a general guideline for data users interested in 
-    assessing the relative reliability of historical species identifications 
-    on these surveys. ", 
-  # "This dataset includes an identification confidence matrix 
-  # for all fishes and invertebrates identified ", 
-  # metadata_sentence_survey_institution, 
-  "Quality Codes: ", 
-  "1: High confidence and consistency. Taxonomy is stable and reliable at this level, and field identification characteristics are well known and reliable. ",
-  "2: Moderate confidence. Taxonomy may be questionable at this level, or field identification characteristics may be variable and difficult to assess consistently. ", 
-  "3: Low confidence. Taxonomy is incompletely known, or reliable field identification characteristics are unknown. ", 
-  "NA: Unassessed. Taxonomy quality has not been assessed. ")#, 
-# metadata_sentence_legal_restrict, " ",  
-# metadata_sentence_github, " ", 
-# metadata_sentence_codebook, " ", 
-# metadata_sentence_last_updated)
+fluctuated over the history of the surveys due to changing priorities and 
+resources. The matrix lists a confidence level for each taxon for each survey 
+year and is intended to serve as a general guideline for data users interested 
+in assessing the relative reliability of historical species identifications on 
+these surveys. Quality Codes: ", 
+  "1: High confidence and consistency. Taxonomy is stable and reliable at this 
+level, and field identification characteristics are well known and reliable. ",
+  "2: Moderate confidence. Taxonomy may be questionable at this level, or field 
+identification characteristics may be variable and difficult to assess 
+  consistently. ", 
+  "3: Low confidence. Taxonomy is incompletely known, or reliable field 
+identification characteristics are unknown. ", 
+  "NA: Unassessed. Taxonomy quality has not been assessed. ")
 
-write.csv(x = TAXONOMIC_CONFIDENCE,
-          file = paste0("temp/taxonomics_confidence.csv"),
-          row.names = F)
+metadata_column <- 
+  RODBC::sqlQuery(channel = channel, 
+                  query = paste(
+                    "SELECT * FROM GAP_PRODUCTS.METADATA_COLUMN
+                WHERE METADATA_COLNAME IN", 
+                    gapindex::stitch_entries(names(x = TAXONOMIC_CONFIDENCE) )
+                  )
+  )
+names(x = metadata_column) <- gsub(x = tolower(x = names(x = metadata_column)),
+                                   pattern = "metadata_", 
+                                   replacement = "")
 
-write.csv(x = TAXONOMIC_CONFIDENCE_COMMENT,
-          file = paste0("temp/taxonomics_confidence.txt"),
-          row.names = F)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  Upload table: gapindex function will drop the table if it already exists,
+##  saves the table, then adds the comment on the table and each column.
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+gapindex::upload_oracle(x = TAXONOMIC_CONFIDENCE, 
+                        schema = "GAP_PRODUCTS", 
+                        table_name = "TAXONOMIC_CONFIDENCE", 
+                        metadata_column = metadata_column, 
+                        table_metadata = TAXONOMIC_CONFIDENCE_COMMENT, 
+                        channel = channel,
+                        share_with_all_users = TRUE)
