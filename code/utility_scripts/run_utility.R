f1dcdad3f04212aede9f3e3a50062f436d00e20a
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-05-18 17:32:27 -0700

    added push_oracle and create_audit_tables r scripts

--- code/utility_scripts/run_utility.R
+++ code/utility_scripts/run_utility.R
@@ -1,12 +1,15 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Project:       GAP_PRODUCTS standard table production workflow
+## Project:       GAP_PRODUCTS Utility Scripts
 ##                NOAA AFSC GAP Survey Team
-##                Utility tasks 
 ## PoC:           Zack Oyafuso (zack.oyafuso@noaa.gov)
 ##                Emily Markowitz (emily.markowitz@noaa.gov)
 ##                
-## Description:   This script houses R scripts used to update certain tables 
-##                within the GAP_PRODUCTS schema as needed. 
+## Description:   This script houses a hodgepodge of R scripts used to perform
+##                miscellaneous functions to maintain the GAP_PRODUCTS 
+##                Oracle schema outside of the standard production run of 
+##                CPUE, Biomass, Size Composition, and Age Composition. In
+##                each section header, I put the frequency at which one would
+##                need to run each script. 
 ##                
 ##                **DISCLAIMER**: Each script is self-contained. Do not source 
 ##                this script. The script for each step needs to be run 
@@ -16,7 +19,7 @@
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Update Metadata Tables ----
-##   Currently Done As Needed
+##   Frequency: As Needed
 ##   metadata.R script updates the metadata tables in GAP_PRODUCTS used to 
 ##   create the metadata for the tables and views in GAP_PRODUCTS. 
 ##   pull_support_tables.R updates the AREA, SURVEY_DESIGN, STRATUM_GROUPS, 
@@ -36,13 +39,23 @@ file.edit("code/utility_scripts/pull_support_tables.R")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Update GAP_PRODUCTS.Taxonomic_Confidence Table ----
-##   Once a year prior to survey to populate taxonomic confidence values for
-##   the current survey year
+##   Frequncy: Once a year prior to survey 
+##   Populates opulate taxonomic confidence values for the current survey year
+##   for FOSS purposes. 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 file.edit("code/utility_scripts/taxonomic_confidence.R")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Update GAP_PRODUCTS.TAXON_GROUPS Table ----
-##   Once a year prior to survey or as needed to update any taxonomic changes
+##   Update GAP_PRODUCTS.TAXONONOIC_GROUPS Table ----
+##   Frequency: Once a year prior to survey or as needed
+##   Updates any taxonomic changes to the taxonomic grouping table
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 file.edit("code/utility_scripts/taxonomic_groupings.R")
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##   In case you need to repush the entire production tables and set up 
+##   table constraints, audit tables and triggers. 
+##   Frequency: As Needed (mostly in case of emergency)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+file.edit("code/push_oracle.R")
+file.edit("code/utility_scripts/create_audit_tables.R")
