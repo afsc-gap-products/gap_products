f820e4383805c9bee92141fe63f5c2fd795c8401
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2024-09-08 17:45:36 -0700

    Moved these scripts to archive

--- code/utility_scripts/run_utility.R
+++ code/utility_scripts/run_utility.R
@@ -18,25 +18,6 @@
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Update Metadata Tables ----
-##   Frequency: As Needed
-##   metadata.R script updates the metadata tables in GAP_PRODUCTS used to 
-##   create the metadata for the tables and views in GAP_PRODUCTS. 
-##   pull_support_tables.R updates the AREA, SURVEY_DESIGN, STRATUM_GROUPS, 
-##   and SPECIES_YEAR tables in GAP_PRODUCTS used to create the
-##   metadata for the tables and views in GAP_PRODUCTS. The contents of these 
-##   tables are maintained in a shared googlesheets document. These tables are 
-##   then uploaded to the GAP_PRODUCTS Oracle schema. A future goal of this 
-##   step is to move away from making changes to the googlesheet and instead 
-##   setting up triggers in Oracle to provide an audit record any time a change
-##   is made to these metadata tables. In this way, changes are arguably 
-##   better documented and the upkeep of the tables are fully contained within
-##   Oracle instead of the current workflow which is Google Sheets --> R (via 
-##   the googledrive R package) --> Oracle. 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-file.edit("code/utility_scripts/pull_support_tables.R")
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Update GAP_PRODUCTS.Taxonomic_Confidence Table ----
 ##   Frequncy: Once a year prior to survey 
 ##   Populates opulate taxonomic confidence values for the current survey year
@@ -52,9 +33,7 @@ file.edit("code/utility_scripts/taxonomic_confidence.R")
 file.edit("code/utility_scripts/taxonomic_groupings.R")
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   In case you need to repush the entire production tables and set up 
-##   table constraints, audit tables and triggers. 
+##   In case you need recreate audit tables. 
 ##   Frequency: As Needed (mostly in case of emergency)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-file.edit("code/utility_scripts/push_oracle.R")
 file.edit("code/utility_scripts/create_audit_tables.R")
