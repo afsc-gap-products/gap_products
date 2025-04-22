a50d7abf281942925418b10d0d565d734503e255
Emily.Markowitz <Emily.Markowitz@nmfs.local>
2025-04-22 15:11:00 -0700

    reran docs and fixed errors

--- content/functions.R
+++ content/functions.R
@@ -3,7 +3,6 @@
 PKG <- c(
   # Keeping Organized
   "devtools", # Package development tools for R; used here for downloading packages from GitHub
-  "distill",
   "gapindex", # devtools::install_github("afsc-gap-products/gapindex")
   "akgfmaps", # devtools::install_github("afsc-gap-products/akgfmaps")
   "dplyr",
@@ -22,17 +21,46 @@ PKG <- c(
   "ftExtra", 
   "RODBC",
   "DBI",
+  "odbc",
   "akfingapdata" # devtools::install_github("MattCallahan-NOAA/akfingapdata")
 )
 
-PKG <- unique(PKG)
-for (p in PKG) {
-  # library(p, character.only = TRUE)
-  if (!require(p, character.only = TRUE)) {
-    install.packages(p)
-    require(p, character.only = TRUE)
+#' Package install
+#' 
+#' @param p name of package
+#' On google workstations you can use the library install bash script Elizabeth created. 
+#' Copy that into you working
+#' directory, run 'chmod u+x ubuntu_libraries.sh' and then run './ubuntu_libraries.sh'
+#' @example 
+#' pkgs <- c("dplyr, "nwfscSurvey", "sdmTMB)
+#' base::lapply(pkgs, pkg_install)
+#' 
+pkg_install <- function(p){
+  if(grepl("/home/user/", getwd())){
+    system("chmod a+x ubuntu_libraries.sh")
+    system("./ubuntu_libraries.sh")
   }
+  if(!require(p, character.only = TRUE)) {
+    if (p == 'coldpool') {
+      devtools::install_github("afsc-gap-products/coldpool")
+    } else if (p == "akgfmapas") {
+      devtools::install_github("afsc-gap-products/akgfmaps", build_vignettes = TRUE)
+    } else if (p == 'nwfscSurvey') {
+      remotes::install_github("pfmc-assessments/nwfscSurvey")
+    } else if (p == "gapctd") {
+      devtools::install_github("afsc-gap-products/gapctd")
+    } else if (p == 'gapindex') {
+      remotes::install_github("afsc-gap-products/gapindex")
+    } else if (p == 'akfingapdata') {
+      # remotes::install_github("afsc-gap-products/akfingapdata")
+      devtools::install_github("MattCallahan-NOAA/akfingapdata")
+    } else {
+      install.packages(p)
+    }
+    require(p, character.only = TRUE)}
 }
+
+base::lapply(unique(PKG), pkg_install)
 
 # knowns -----------------------------------------------------------------------
 
