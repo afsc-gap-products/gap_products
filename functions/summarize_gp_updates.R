0e4bace8300a03975fc4dcd0e3b7fdb75a6618a9
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-03-14 14:21:30 -0700

    adding functionality to odbc

--- functions/summarize_gp_updates.R
+++ functions/summarize_gp_updates.R
@@ -12,9 +12,9 @@ summarize_gp_updates <- function(channel = NULL,
   if (is.null(x = time_start)) stop("Must provide time_start.")
   parsed_date <- strptime(time_start, "%d-%b-%y %I.%M.%S %p")
   if (is.na(x = parsed_date)) stop("Must provide time_start in format 'dd-MON-YY HH.MM.SS AM/PM', e.g., 08-SEP-24 12.00.00 PM")
-    
+  
   if (is.null(x = time_end)) time_end <-
-      format(x = Sys.time(), format = "%d-%b-%y %I.%M.%S %p")
+    format(x = Sys.time(), format = "%d-%b-%y %I.%M.%S %p")
   parsed_date <- strptime(time_end, "%d-%b-%y %I.%M.%S %p")
   if (is.na(x = parsed_date)) stop("Must provide time_start in format 'DD-MON-YY HH.MM.SS AM/PM', e.g., 08-SEP-24 12.00.00 PM")
   
@@ -57,7 +57,7 @@ FROM GAP_ARCHIVE.AUDIT_AGECOMP
 WHERE OPERATION_TIMESTAMP BETWEEN '", time_start, "' AND '", time_end, "'",
     "\nGROUP BY OPERATION_TYPE, SURVEY_DEFINITION_ID, YEAR")
   
-  return(RODBC::sqlQuery(channel = channel,
-                         query = sql_query))
+  return(gapindex::sql_query(channel = channel,
+                             query = sql_query))
 }
 
