73c2560408bc97039eee07300e3578335015b91c
Em Markowitz (NOAA) <75965120+EmilyMarkowitz-NOAA@users.noreply.github.com>
2026-05-12 22:21:54 -0700

    Update and rename AKFIN_SPECIMEN.sql to SPECIMEN.sql

--- code/sql_akfin/SPECIMEN.sql
+++ code/sql_akfin/SPECIMEN.sql
@@ -1,3 +1,14 @@
+-- SQL Command to Create Materialized View GAP_PRODUCTS.SPECIMEN
+--
+-- Created by querying records from RACEBASE.SPECIMEN but only using hauls 
+-- with ABUNDANCE_HAUL = 'Y' from the five survey areas w/ survey_definition_id:
+-- "AI" = 52, "GOA" = 47, "EBS" = 98, "BSS" = 78, "NBS" = 143
+--
+-- Contributors: Ned Laman (ned.laman@noaa.gov), 
+--               Zack Oyafuso (zack.oyafuso@noaa.gov), 
+--               Emily Markowitz (emily.markowitz@noaa.gov)
+--
+
 -- SQL Command to Create Materilized View GAP_PRODUCTS.AKFIN_SPECIMEN
 --
 -- Created by querying records from RACEBASE.SPECIMEN but only using hauls 
@@ -9,7 +20,7 @@
 --               Emily Markowitz (emily.markowitz@noaa.gov)
 --
 
-CREATE MATERIALIZED VIEW GAP_PRODUCTS.AKFIN_SPECIMEN AS 
+CREATE MATERIALIZED VIEW GAP_PRODUCTS.SPECIMEN AS
 -- Part 1: Length Frequency Data
 SELECT 
     ABS(B.HAUL_ID) AS HAULJOIN,
@@ -58,3 +69,41 @@ FROM RACEBASE.SPECIMEN ss
 RIGHT JOIN RACEBASE.HAUL hh 
 ON ss.HAULJOIN = hh.HAULJOIN
 WHERE hh.ABUNDANCE_HAUL = 'Y'
+
+
+-- Code for GAP_PRODUCTS.SPECIMEN for posterity 2026-05-12
+
+-- CREATE MATERIALIZED VIEW GAP_PRODUCTS.SPECIMEN AS
+-- SELECT 
+--     h.HAULJOIN,
+--     s.SPECIMENID                 AS SPECIMEN_ID,
+--     s.SPECIES_CODE,
+--     s.LENGTH                     AS LENGTH_MM,
+--     s.SEX,
+--     s.WEIGHT                     AS WEIGHT_G,
+--     s.AGE,
+--     s.MATURITY,
+--     s.GONAD_WT                   AS GONAD_G,
+--     s.SPECIMEN_SUBSAMPLE_METHOD,
+--     s.SPECIMEN_SAMPLE_TYPE,
+--     s.AGE_DETERMINATION_METHOD 
+-- FROM RACEBASE.SPECIMEN s
+-- JOIN RACEBASE.HAUL h 
+--     ON h.HAULJOIN = s.HAULJOIN
+-- JOIN RACE_DATA.CRUISES c 
+--     ON c.RACEBASE_CRUISEJOIN = h.CRUISEJOIN
+-- JOIN RACE_DATA.SURVEYS surv 
+--     USING (SURVEY_ID)
+-- WHERE h.ABUNDANCE_HAUL = 'Y'
+--   AND surv.SURVEY_DEFINITION_ID IN (143, 98, 47, 52, 78);
+  
+-- Original code for GAP_PRODUCTS.AKFIN_SPECIMEN for posterity 2026-05-10
+  
+-- CREATE MATERIALIZED VIEW GAP_PRODUCTS.AKFIN_SPECIMEN AS 
+-- SELECT HAULJOIN, SPECIMENID AS SPECIMEN_ID, SPECIES_CODE, LENGTH AS LENGTH_MM, 
+-- SEX, WEIGHT AS WEIGHT_G, AGE, MATURITY, GONAD_WT AS GONAD_G, 
+-- SPECIMEN_SUBSAMPLE_METHOD, SPECIMEN_SAMPLE_TYPE, AGE_DETERMINATION_METHOD 
+-- 
+-- FROM RACEBASE.SPECIMEN 
+-- JOIN RACEBASE.HAUL USING (HAULJOIN)
+-- WHERE RACEBASE.HAUL.ABUNDANCE_HAUL = 'Y'
