35d23a7f6c6d3d2db606cc2fb8625248880d7eb7
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-05-22 08:49:04 -0700

    initial 2021-2024 historical data push

--- code/ctd_data/append_ctd_lookup_tables.R
+++ code/ctd_data/append_ctd_lookup_tables.R
@@ -5,6 +5,7 @@
 
 rm(list = ls())
 
+## Connect to Oracle
 library(readxl)
 library(gapindex)
 
@@ -19,3 +20,25 @@ for (itable in c("CTD_VARIABLE_CODES", "CTD_INSTRUMENT_CODES",
   DBI::dbAppendTable(conn = chl, name = itable, value = lookup)
 }
 
+## Upload historical CTD data from 2021-2024
+ctd_data_2021_2024 <- 
+  readRDS(file = "code/ctd_data/GAPCTD_all_casts_2021_2024.rds")
+names(x = ctd_data_2021_2024) <- toupper(x = names(x = ctd_data_2021_2024))
+
+DBI::dbAppendTable(conn = chl, 
+                   name =  "CTD_DATA", 
+                   value = subset(x = ctd_data_2021_2024,
+                                  subset =  DEPTH_M >= 0))
+
+# for(i in 1:nrow(ctd_data_2021_2024)) {
+for (i in 1124001:1125000) {  
+  tryCatch({
+    DBI::dbAppendTable(conn = chl, 
+                       name =  "CTD_DATA", 
+                       value = ctd_data_2021_2024[i, ])
+    
+  }, error = function(e) {
+    cat("Error occurred on row:", i, "\n")
+    print(ctd_data_2021_2024[i, ])
+  })
+}
