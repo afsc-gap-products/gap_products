f9de98b67ee0c042aebf55dbf4017e2763825d75
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-05-22 11:22:18 -0700

    replace numeric null with NA

--- code/ctd_data/append_ctd_lookup_tables.R
+++ code/ctd_data/append_ctd_lookup_tables.R
@@ -13,7 +13,7 @@ chl <- gapindex::get_connected(conn_type = "DBI", check_access = FALSE)
 
 ## Loop over the lookup tables and append to the tables created in Oracle
 for (itable in c("CTD_VARIABLE_CODES", "CTD_INSTRUMENT_CODES", 
-           "CTD_DIRECTION_CODES")) {
+                 "CTD_DIRECTION_CODES")) {
   lookup <- readxl::read_xlsx(path = "code/ctd_data/ctd_lookup_table_data.xlsx", 
                               sheet = itable)
   
@@ -23,22 +23,14 @@ for (itable in c("CTD_VARIABLE_CODES", "CTD_INSTRUMENT_CODES",
 ## Upload historical CTD data from 2021-2024
 ctd_data_2021_2024 <- 
   readRDS(file = "code/ctd_data/GAPCTD_all_casts_2021_2024.rds")
+
+## Replace numeric null with NA
+ctd_data_2021_2024[ctd_data_2021_2024 == -2147483647] <- NA
 names(x = ctd_data_2021_2024) <- toupper(x = names(x = ctd_data_2021_2024))
 
-DBI::dbAppendTable(conn = chl, 
-                   name =  "CTD_DATA", 
-                   value = subset(x = ctd_data_2021_2024,
-                                  subset =  DEPTH_M >= 0))
-
-# for(i in 1:nrow(ctd_data_2021_2024)) {
-for (i in 1124001:1125000) {  
-  tryCatch({
-    DBI::dbAppendTable(conn = chl, 
-                       name =  "CTD_DATA", 
-                       value = ctd_data_2021_2024[i, ])
-    
-  }, error = function(e) {
-    cat("Error occurred on row:", i, "\n")
-    print(ctd_data_2021_2024[i, ])
-  })
-}
+## upload complete cases
+DBI::dbAppendTable(
+  conn = chl, 
+  name =  "CTD_DATA", 
+  value = ctd_data_2021_2024[complete.cases(ctd_data_2021_2024), ] 
+)
