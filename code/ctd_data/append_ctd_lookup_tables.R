79e54d18138511fde6e53784520ddc673e9812bc
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2026-05-26 12:23:08 -0700

    added instrument data for each hauljoin

--- code/ctd_data/append_ctd_lookup_tables.R
+++ code/ctd_data/append_ctd_lookup_tables.R
@@ -5,11 +5,15 @@
 
 rm(list = ls())
 
-## Connect to Oracle
 library(readxl)
 library(gapindex)
 
+## Connect to Oracle
 chl <- gapindex::get_connected(conn_type = "DBI", check_access = FALSE)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  Upload Lookup Tables
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Loop over the lookup tables and append to the tables created in Oracle
 for (itable in c("CTD_VARIABLE_CODES", "CTD_INSTRUMENT_CODES", 
@@ -20,17 +24,25 @@ for (itable in c("CTD_VARIABLE_CODES", "CTD_INSTRUMENT_CODES",
   DBI::dbAppendTable(conn = chl, name = itable, value = lookup)
 }
 
-## Upload historical CTD data from 2021-2024
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  Upload historical CTD data from 2021-2024
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ctd_data_2021_2024 <- 
   readRDS(file = "code/ctd_data/GAPCTD_all_casts_2021_2024.rds")
-
-## Replace numeric null with NA
-ctd_data_2021_2024[ctd_data_2021_2024 == -2147483647] <- NA
 names(x = ctd_data_2021_2024) <- toupper(x = names(x = ctd_data_2021_2024))
 
-## upload complete cases
-DBI::dbAppendTable(
-  conn = chl, 
-  name =  "CTD_DATA", 
-  value = ctd_data_2021_2024[complete.cases(ctd_data_2021_2024), ] 
-)
+DBI::dbAppendTable(conn = chl, 
+                   name =  "CTD_DATA", 
+                   value = ctd_data_2021_2024)
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  Upload Instrument Data
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ctd_instrument_data_2021_2024 <- 
+  readRDS(file = "code/ctd_data/GAPCTD_instrument_2021_2024.rds")
+names(x = ctd_instrument_data_2021_2024) <- 
+  toupper(x = names(x = ctd_instrument_data_2021_2024))
+
+DBI::dbAppendTable(conn = chl, 
+                   name =  "CTD_HAUL_INSTRUMENT", 
+                   value = ctd_instrument_data_2021_2024)
