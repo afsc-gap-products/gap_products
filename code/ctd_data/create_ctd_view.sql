2eacd40d145026021556e5c8abb6a8b087937c74
Em Markowitz (NOAA) <75965120+EmilyMarkowitz-NOAA@users.noreply.github.com>
2026-08-18 23:05:53 -0700

    1. removed "CTD_" prefixes from table names and column names, 2. removed CTD_INSTRUMENT_CODES table creation code and references, 3. folded instrument codes into the ENVIRONMENTAL table (previously CTD_DATA), 4. changed the VAR_NAME column to NAME, in keeping with our naming definitions in METADATA_COLUMN table, 5. updated the create_ctd_view.sql table to match the new column names and table names, 6. updated fields in ctd_lookup_table_data.xlsx to match new column names.

--- code/ctd_data/create_ctd_view.sql
+++ code/ctd_data/create_ctd_view.sql
@@ -1,27 +1,27 @@
-CREATE OR REPLACE VIEW CTD_SUMMARY_V AS 
+CREATE OR REPLACE VIEW ENVIRONMENTAL_SUMMARY_V AS 
 SELECT * FROM (
     SELECT 
         d.hauljoin, 
         d.depth_m, 
         -- Convert DO in mmols/L to mL/L 
         CASE 
-            WHEN c.var_name IN ('DOXY_ML_L', 'DOXY_MMOL_L') THEN 'DOXY_ML_L' 
-            ELSE c.var_name 
-        END AS var_name,
+            WHEN c.name IN ('DOXY_ML_L', 'DOXY_MMOL_L') THEN 'DOXY_ML_L' 
+            ELSE c.name 
+        END AS name,
         CASE 
-            WHEN c.var_name = 'DOXY_MMOL_L' THEN d.value / 44.6596
+            WHEN c.name = 'DOXY_MMOL_L' THEN d.value / 44.6596
             
             ELSE d.value 
         END AS value
-    FROM CTD_DATA d
-    JOIN CTD_VARIABLE_CODES c ON d.variable = c.variable
+    FROM ENVIRONMENTAL d
+    JOIN VARIABLE_CODES c ON d.variable = c.variable
     WHERE d.direction = 0
-      AND c.var_name IN ('TEMPERATURE_C', 'SALINITY_PSS78', 'PH', 
+      AND c.name IN ('TEMPERATURE_C', 'SALINITY_PSS78', 'PH', 
                          'DOXY_ML_L', 'DOXY_MMOL_L')
 )
 PIVOT (
     MAX(value) 
-    FOR var_name IN (
+    FOR name IN (
         'TEMPERATURE_C'    AS TEMPERATURE_C,
         'SALINITY_PSS78'   AS SALINITY_PSS78,
         'PH'               AS PH,
