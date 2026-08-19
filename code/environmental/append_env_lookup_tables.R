af36a410ceb1f2224eebe632343cb11da192027a
Em Markowitz (NOAA) <75965120+EmilyMarkowitz-NOAA@users.noreply.github.com>
2026-08-18 23:12:34 -0700

    renamed the ctd_data folder to environmental, changed the file names from ctd -> env, and updated paths within the append_env_lookup_tables.R script

--- code/environmental/append_env_lookup_tables.R
+++ code/environmental/append_env_lookup_tables.R
@@ -18,7 +18,7 @@ chl <- gapindex::get_connected(conn_type = "DBI", check_access = FALSE)
 ## Loop over the lookup tables and append to the tables created in Oracle
 for (itable in c("VARIABLE_CODES", #"INSTRUMENT_CODES", 
                  "DIRECTION_CODES")) {
-  lookup <- readxl::read_xlsx(path = "code/ctd_data/ctd_lookup_table_data.xlsx", 
+  lookup <- readxl::read_xlsx(path = "code/environmental/env_lookup_table_data.xlsx", 
                               sheet = itable)
   
   DBI::dbAppendTable(conn = chl, name = itable, value = lookup)
@@ -27,17 +27,17 @@ for (itable in c("VARIABLE_CODES", #"INSTRUMENT_CODES",
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##  Upload historical CTD data from 2021-2024
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-env <- readRDS(file = "code/ctd_data/GAPCTD_all_casts_2021_2024.rds")
+env <- readRDS(file = "code/environmental/GAPCTD_all_casts_2021_2024.rds")
 names(x = env) <- toupper(x = names(x = env))
 
-instruments <- readRDS(file = "code/ctd_data/GAPCTD_instrument_2021_2024.rds")
+instruments <- readRDS(file = "code/environmental/GAPCTD_instrument_2021_2024.rds")
 names(x = instruments) <- toupper(x = names(x = instruments))
 
 env <- merge(
   x = env, 
   y = instruments, 
   by = "HAULJOIN", 
-  all.x = TRUE # Keeps all rows from ctd_data even if a hauljoin is missing in the instrument table
+  all.x = TRUE # Keeps all rows from environmental even if a hauljoin is missing in the instrument table
 )
 env <- env[, c("HAULJOIN", "DIRECTION", "DEPTH_M", "INSTRUMENT", "VARIABLE", "VALUE")]
 
@@ -49,7 +49,7 @@ DBI::dbAppendTable(conn = chl,
 # ##  Upload Instrument Data
 # ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ctd_instrument_data_2021_2024 <- 
-#   readRDS(file = "code/ctd_data/GAPCTD_instrument_2021_2024.rds")
+#   readRDS(file = "code/environmental/GAPCTD_instrument_2021_2024.rds")
 # names(x = ctd_instrument_data_2021_2024) <- 
 #   toupper(x = names(x = ctd_instrument_data_2021_2024))
 # 
